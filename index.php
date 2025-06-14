8c58379aaedbdbf5bb03e7861a435be4455366f2
KaviduDilshan <dilshank404@gmail.com>
2025-06-14 15:20:11 +0530

    update

--- index.php
+++ index.php
@@ -10,13 +10,14 @@ include_once './conn.php';
 <!-- Mirrored from themesbrand.com/velzon/html/master/auth-signin-basic.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 12 Aug 2024 06:11:16 GMT -->
 
 <head>
-<?php
-if (isset($_GET['error'])) {
-    $error = $_GET['error'];
-} else {
-    $error = '';
-}
-?>
+    <?php
+    if (isset($_GET['error'])) {
+        $error = $_GET['error'];
+    } else {
+        $error = '';
+    }
+    ?>
+
 <body>
 
     <meta charset="utf-8" />
@@ -39,150 +40,129 @@ if (isset($_GET['error'])) {
     <!-- custom Css-->
     <link href="assets/css/custom.min.css" rel="stylesheet" type="text/css" />
 
-</head>
+    </head>
 
-<body>
+    <body>
 
-    <div class="auth-page-wrapper pt-5">
-        <!-- auth page bg -->
-        <div class="auth-one-bg-position auth-one-bg" id="auth-particles">
-            <div class="bg-overlay"></div>
+        
+            <!-- auth page content -->
+            <div class="auth-page-content" style="background-image: linear-gradient(to right, #1A2980 0%, #2955b4 51%, #298bdb 100%); height:100vh; " >
+                <div class="container" >
 
-            <div class="shape">
-                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink"
-                    viewBox="0 0 1440 120">
-                    <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
-                </svg>
-            </div>
-        </div>
+                    <div class="row justify-content-center ">
+                        <div class="col-md-10  col-lg-6 col-xl-7">
+                            <div class="card login-card card-bg-fill " style="border-radius: 10px;">
+                                <div class="card-body p-4 ">
+                                    <div class="text-center mt-2">
+                                        <h4 class="text-primary" style="font-size:50px">JMK Enterprises</h4>
+                                        <p class="text-muted" style="font-size:20px">Sign in to continue</p>
+                                    </div>
+                                    <div class="p-2 mt-4">
+                                        <form action="data/data_login.php" method="post">
 
-        <!-- auth page content -->
-        <div class="auth-page-content">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="text-center mt-sm-5 mb-4 text-white-50 heding">
-                            <h2>WELCOME!</h2>
-                                <!-- <h2 style="color:white;">JMK Enterprises</h2> -->
-                                <!-- <a href="index.html" class="d-inline-block auth-logo">
-                                    <img src="assets/images/logo-light.png" alt="" height="20">
-                                </a> -->
-                            <!-- <p class="mt-3 fs-15 fw-medium">Premium Admin & Dashboard Template</p> -->
-                        </div>
-                    </div>
-                </div>
-                <!-- end row -->
+                                            <?php if ($error != '') { ?>
+                                                <div class="row">
+                                                    <div id="error_display" class=" text-danger">
+                                                        <?php
+                                                        if ($error == '0') {
 
-                <div class="row justify-content-center ">
-                    <div class="col-md-9  col-lg-6 col-xl-5">
-                        <div class="card login-card card-bg-fill ">
-                            <div class="card-body p-4 ">
-                                <div class="text-center mt-2">
-                                    <h4 class="text-primary" style="font-size:40px">JMK Enterprises</h4>
-                                    <p class="text-muted" style="font-size:20px">Sign in to continue</p>
-                                </div>
-                                <div class="p-2 mt-4">
-                                    <form action="data/data_login.php" method="post">
+                                                            echo "Please fill-in the Username and Password";
+                                                        } else if ($error == '1') {
+                                                            echo '<script>  swal("Invalid Username/Password", "Account not Active", "error");</script>';
+                                                            echo "Invalid Username /Password or Account not Active.";
+                                                        } else if ($error == '5') {
+                                                            echo '<script>  swal("Invalid Captha Code", "Please Try Agin", "warning");</script>';
+                                                            echo "Invalid Captha Code";
+                                                        } else if ($error == 6) {
+                                                            echo '<script>  swal("Security Alert", "Multiple Login", "warning");</script>';
+                                                            echo "Security Alert";
+                                                        }
+                                            }
+                                            ?>
 
-                                    <?php if ($error != '') { ?>
-                                            <div class="row">
-                                                <div id="error_display" class=" text-danger">
-                                                    <?php
-                                                    if ($error == '0') {
+                                                    <div class="mb-3">
+                                                        <label for="username" class="form-label"
+                                                            style="font-size:20px">Username</label>
+                                                        <input type="text" id="a_username" name="a_username"
+                                                            class="form-control" placeholder="Enter username">
+                                                    </div>
 
-                                                        echo "Please fill-in the Username and Password";
-                                                    } else if ($error == '1') {
-                                                        echo '<script>  swal("Invalid Username/Password", "Account not Active", "error");</script>';
-                                                        echo "Invalid Username /Password or Account not Active.";
-                                                    } else if ($error == '5') {
-                                                        echo '<script>  swal("Invalid Captha Code", "Please Try Agin", "warning");</script>';
-                                                        echo "Invalid Captha Code";
-                                                    } else if ($error == 6) {
-                                                        echo '<script>  swal("Security Alert", "Multiple Login", "warning");</script>';
-                                                        echo "Security Alert";
-                                                    }
-                                                }
-                                                    ?>
-
-                                        <div class="mb-3">
-                                            <label for="username" class="form-label" style="font-size:15px">Username</label>
-                                            <input type="text" id="a_username" name="a_username" class="form-control"
-                                                placeholder="Enter username">
-                                        </div>
-
-                                        <div class="mb-3">
-                                            <!-- <div class="float-end">
+                                                    <div class="mb-3">
+                                                        <!-- <div class="float-end">
                                                 <a href="auth-pass-reset-basic.html" class="text-muted">Forgot password?</a>
                                             </div> -->
-                                            <label class="form-label" for="password-input"style="font-size:15px" >Password</label>
-                                            <div class="position-relative auth-pass-inputgroup mb-3">
-                                                <input type="password" id="a_password" name="a_password"
-                                                    class="form-control pe-5 password-input"
-                                                    placeholder="Enter password">
-                                                <button
-                                                    class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon material-shadow-none"
-                                                    type="button" id="password-addon"><i
-                                                        class="ri-eye-fill align-middle"></i></button>
-                                            </div>
-                                        </div>
+                                                        <label class="form-label" for="password-input"
+                                                            style="font-size:20px">Password</label>
+                                                        <div class="position-relative auth-pass-inputgroup mb-3">
+                                                            <input type="password" id="a_password" name="a_password"
+                                                                class="form-control pe-5 password-input"
+                                                                placeholder="Enter password">
+                                                            <button
+                                                                class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon material-shadow-none"
+                                                                type="button" id="password-addon"><i
+                                                                    class="ri-eye-fill align-middle"></i></button>
+                                                        </div>
+                                                    </div>
 
-                                        <!-- <div class="form-check">
+                                                    <!-- <div class="form-check">
                                             <input class="form-check-input" type="checkbox" value="" id="auth-remember-check">
                                             <label class="form-check-label" for="auth-remember-check">Remember me</label>
                                         </div> -->
 
-                                        <div class="mt-4">
-                                            <button class="btn btn-success w-100" type="submit">LogIn</button>
-                                        </div>
+                                                    <div class="mt-4">
+                                                        <button class="btn btn-success w-100"
+                                                            type="submit" style="font-size:20px">LogIn</button>
+                                                    </div>
 
-                                    </form>
+                                        </form>
+                                    </div>
                                 </div>
+                                <!-- end card body -->
                             </div>
-                            <!-- end card body -->
-                        </div>
-                        <!-- end card -->
+                            <!-- end card -->
 
+                        </div>
+                    </div>
+                    <!-- end row -->
+                </div>
+                <!-- end container -->
+            </div>
+            <!-- end auth page content -->
+
+            <!-- footer -->
+            <footer class="footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="text-center">
+                                <p class="mb-0 text-muted">&copy;
+                                    <script>document.write(new Date().getFullYear())</script> JMK Enterprises. Developed
+                                    by
+                                    <a href="www.tritcal.com">Tritcal International (Pvt.) Ltd</a>
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <!-- end row -->
-            </div>
-            <!-- end container -->
+            </footer>
+            <!-- end Footer -->
         </div>
-        <!-- end auth page content -->
+        <!-- end auth-page-wrapper -->
 
-        <!-- footer -->
-        <footer class="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="text-center">
-                            <p class="mb-0 text-muted">&copy;
-                                <script>document.write(new Date().getFullYear())</script> JMK Enterprises. Developed by
-                                <a href="www.tritcal.com">Tritcal International (Pvt.) Ltd</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-        <!-- end Footer -->
-    </div>
-    <!-- end auth-page-wrapper -->
+        <!-- JAVASCRIPT -->
+        <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/libs/simplebar/simplebar.min.js"></script>
+        <script src="assets/libs/node-waves/waves.min.js"></script>
+        <script src="assets/libs/feather-icons/feather.min.js"></script>
+        <script src="assets/js/pages/plugins/lord-icon-2.1.0.js"></script>
+        <script src="assets/js/plugins.js"></script>
 
-    <!-- JAVASCRIPT -->
-    <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/libs/simplebar/simplebar.min.js"></script>
-    <script src="assets/libs/node-waves/waves.min.js"></script>
-    <script src="assets/libs/feather-icons/feather.min.js"></script>
-    <script src="assets/js/pages/plugins/lord-icon-2.1.0.js"></script>
-    <script src="assets/js/plugins.js"></script>
-
-    <!-- particles js -->
-    <script src="assets/libs/particles.js/particles.js"></script>
-    <!-- particles app js -->
-    <script src="assets/js/pages/particles.app.js"></script>
-    <!-- password-addon init -->
-    <script src="assets/js/pages/password-addon.init.js"></script>
-</body>
+        <!-- particles js -->
+        <script src="assets/libs/particles.js/particles.js"></script>
+        <!-- particles app js -->
+        <script src="assets/js/pages/particles.app.js"></script>
+        <!-- password-addon init -->
+        <script src="assets/js/pages/password-addon.init.js"></script>
+    </body>
 
 </html>
