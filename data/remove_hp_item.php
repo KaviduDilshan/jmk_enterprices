e58a1b0992322ae9eab35b240e9cf7d698afcc6d
Pubudu Ishantha <pubudu@tritcal.com>
2025-06-19 13:55:57 +0530

    k

---
+++ data/remove_hp_item.php
@@ -0,0 +1,31 @@
+<?php
+
+session_start();
+include_once '../../conn.php';
+
+if (isset($_GET["hp_sales_id"]) && isset($_GET["invoice"]) && isset($_SESSION['login'])) {
+    $hp_sales_id = base64_decode($_GET["hp_sales_id"]);
+    $invoice = $_GET["invoice"];
+    
+
+    $po_det = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM `hp_sales_order` WHERE `hp_sales_id`=$hp_sales_id"));
+    $qty = $po_det["quantity"];
+    $pro_id = $po_det["pro_id"];
+    $company = $po_det['company'];
+    if ($company == 1) {
+        $bra = 'qty_b1';
+    } elseif ($company == 2) {
+        $bra = 'qty_b2';
+    } elseif ($company == 3) {
+        $bra = 'qty_b3';
+    } elseif ($company == 4) {
+        $bra = 'qty_b4';
+    } elseif ($company == 5) {
+        $bra = 'qty_b5';
+    }
+    mysqli_query($conn, "UPDATE products SET $bra = $bra + $qty WHERE pro_id=$pro_id");
+    if ($po_det != null) {
+        mysqli_query($conn, "DELETE FROM `hp_sales_order` WHERE `hp_sales_id`=$hp_sales_id");
+    }
+    header("location:../hp_sales.php?invoice=$invoice");
+}
